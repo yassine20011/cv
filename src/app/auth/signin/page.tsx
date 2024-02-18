333370b0c495ab160dffa97a8b06881fd8c0612c
yassine20011 <medalgamer47@gmail.com>
2024-02-18 01:04:48 +0100

    Remove Header component from signin page

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,5 +1,4 @@
 import { LoginForm } from "./form";
-import Header from "@/components/header";
 import { getServerSession } from "next-auth/next"
 import { redirect  } from 'next/navigation'
 
@@ -13,7 +12,6 @@ export default async function LoginPage() {
 
   return (
     <>
-      <Header />
       <section className="bg-ct-blue-600 min-h-screen pt-20">
         <div className="container mx-auto px-6 py-12 h-full flex justify-center items-center">
           <div className="md:w-8/12 lg:w-5/12 bg-white px-8 py-10">
