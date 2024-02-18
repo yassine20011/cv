42e265de8b700b52290ebf236cb7c0f9713aaa6f
yassine20011 <medalgamer47@gmail.com>
2024-02-18 01:05:14 +0100

    Fix import and add toast notification

--- src/app/personal-details/clientSide.tsx
+++ src/app/personal-details/clientSide.tsx
@@ -1,16 +1,25 @@
 "use client"
-import React from 'react'
-import { useFormStatus } from 'react-dom'
+import { useFormStatus} from 'react-dom'
+import { useEffect, useState } from 'react'
 import { Button } from '@/components/ui/button'
+import { toast } from 'sonner'
+
 
 function Submit() {
 
     const { pending } = useFormStatus()
 
+    useEffect(() => {
+        if(pending) {
+            toast.success("Resume updated successfully")
+        }
+    }, [pending])
+    
+
 
     return (<>
         <Button type="submit" className="w-full mt-4" variant="outline"
-            disabled={pending}
+            disabled={pending} 
         > Update </Button>
 
     </>);
