9d0dc955e8d48edca0b4e80c83d18b8c1c7441b5
yassine20011 <medalgamer47@gmail.com>
2024-02-18 01:01:52 +0100

    Add DELETE route for education API

---
+++ src/app/api/education/[id]/route.tsx
@@ -0,0 +1,45 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+
+
+export async function DELETE(
+    request: Request,
+    { params }: { params: { id: number } }
+) {
+
+    const id = params.id as unknown as string;
+
+    const session = await getServerSession();
+    if (!session) {
+        return new Response(null, { status: 401 });
+    }
+
+
+    try{
+        await prisma.education.delete({
+            where: {
+                id: parseInt(id),
+            },
+        });
+        
+    } catch (e) {
+        console.log(e);
+        return new Response(null, { status: 500 });
+    }
+
+    const user = await prisma.user.findUnique({
+        where: {
+            email: session?.user?.email ?? undefined,
+        },
+    });
+
+    const education = await prisma.education.findMany({
+        where: {
+            resumeId: user?.id,
+        },
+    });
+    
+    return new Response(JSON.stringify(education), { status: 200 });
+
+}
