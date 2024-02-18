ab9aff0afeb5f1b2c42dcf4a1ca1ab1a59cf37d6
yassine20011 <medalgamer47@gmail.com>
2024-02-18 01:05:07 +0100

    Update handleAction function in personal-details/action.tsx

--- src/app/personal-details/action.tsx
+++ src/app/personal-details/action.tsx
@@ -4,7 +4,7 @@ import { getServerSession } from "next-auth/next"
 import { prisma } from "@/lib/prisma"
 import { options } from "../api/auth/[...nextauth]/options"
 
-export const handleAction = async (formdata : FormData) => {
+export const handleAction = async (formdata: FormData) => {
 
     const session = await getServerSession(options)
 
@@ -21,23 +21,31 @@ export const handleAction = async (formdata : FormData) => {
     const skills = formdata.get("skills") as string;
     const name = formdata.get("name") as string;
 
+    try {
 
-    await prisma.resume.update({
-        where: {
-            id: user.id
-        },
-        data: {
-            name: name,
-            subdomain: name.split(" ").join("-").toLowerCase(),
-            initials: name.split(" ").map((n) => n[0]).join(""), // e.g. John Doe -> JD
-            tel: formdata.get("phone") as string,
-            location: formdata.get("location") as string,
-            locationLink: formdata.get("locationLink") as string,
-            about: formdata.get("about") as string,
-            summary: formdata.get("summary") as string,
-            personalWebsiteUrl: formdata.get("portfolio") as string,
-            email: formdata.get("email") as string,
-            skills: skills.split(",").filter((s) => s !== ""), // remove empty strings
-        }
-    })
+        await prisma.resume.update({
+            where: {
+                id: user.id
+            },
+            data: {
+                name: name,
+                subdomain: name.split(" ").join("-").toLowerCase(),
+                initials: name.split(" ").map((n) => n[0]).join(""), // e.g. John Doe -> JD
+                tel: formdata.get("phone") as string,
+                location: formdata.get("location") as string,
+                locationLink: formdata.get("locationLink") as string,
+                about: formdata.get("about") as string,
+                summary: formdata.get("summary") as string,
+                personalWebsiteUrl: formdata.get("portfolio") as string,
+                email: formdata.get("email") as string,
+                skills: skills.split(",").filter((s) => s !== ""), // remove empty strings
+                linkedin: formdata.get("linkedin") as string,
+                github: formdata.get("github") as string,
+                twitter: formdata.get("twitter") as string,
+            }
+        })
+    } catch (error) {
+        console.error(error)
+        throw new Error("Error updating resume")
+    }
 }
