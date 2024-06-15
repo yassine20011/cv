edb5454175bb3820de578d3335ea1886dd7e5102
yassine20011 <medalgamer47@gmaim.com>
2024-06-15 01:19:28 +0100

    Removing query from the console

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient({
-    log: ["query", "info", "warn"],
+    log: ["info", "warn", "error"],
 });
 
 if (process.env.NODE_ENV !== "production") {
