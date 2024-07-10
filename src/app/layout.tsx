83ca2ee0b838a00cbd91619a2b1b84737821d833
yassine20011 <medalgamer47@gmaim.com>
2024-07-10 01:50:55 +0100

    chore: Update @vercel/analytics dependency to version 1.3.1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
-
 import "./globals.css";
 import React from "react";
 import NextAuthProvider from '@/app/auth/NextAuthProvider';
